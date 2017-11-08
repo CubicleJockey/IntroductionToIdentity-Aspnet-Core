0d7f2b15de7f238056611ee282b6adca0e0bbd87
André Davis <CubicleJockey@users.noreply.github.com>
2017-11-08 14:47:30 -0800

    Template for ASP.NET Core  MVC w/ Cleanup

---
+++ IntroToIdentity.AspnetCore.Example/IntroToIdentity.AspnetCore.Example/Models/ManageViewModels/GenerateRecoveryCodesViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace IntroToIdentity.AspnetCore.Example.Models.ManageViewModels
+{
+    public class GenerateRecoveryCodesViewModel
+    {
+        public IEnumerable<string> RecoveryCodes { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # IntroductionToIdentity-Aspnet-Core
+
+* [Introduction Documentation](https://docs.microsoft.com/en-us/aspnet/core/security/authentication/identity?tabs=visual-studio%2Caspnetcore2x)
